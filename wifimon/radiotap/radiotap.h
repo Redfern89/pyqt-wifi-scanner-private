48c3b4ea90e786c1975b9b23d5d43b809ceecb9f
root <root@DESKTOP-78CQKDJ.lan>
2025-02-28 05:21:24 +0300

    новый файл:    wifimon/Makefile 	удалено:       wifimon/radiotap/CMakeLists.txt 	удалено:       wifimon/radiotap/COPYING 	удалено:       wifimon/radiotap/parse.c 	удалено:       wifimon/radiotap/platform.h 	изменено:      wifimon/radiotap/radiotap.c 	изменено:      wifimon/radiotap/radiotap.h 	удалено:       wifimon/radiotap/radiotap_iter.h 	изменено:      wifimon/wifimon.c

--- wifimon/radiotap/radiotap.h
+++ wifimon/radiotap/radiotap.h
@@ -1,211 +1,301 @@
-/*
- * Copyright (c) 2017		Intel Deutschland GmbH
- *
- * Permission to use, copy, modify, and/or distribute this software for any
- * purpose with or without fee is hereby granted, provided that the above
- * copyright notice and this permission notice appear in all copies.
- *
- * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
- * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
- * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
- * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
- * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
- * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
- * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
- */
-#ifndef __RADIOTAP_H
-#define __RADIOTAP_H
+#ifndef IEEE80211_RADIOTAP_H
+#define IEEE80211_RADIOTAP_H
 
-#if defined(__APPLE__)
-#include <libkern/OSByteOrder.h>
-#define bswap_16 OSSwapInt16
-#define bswap_32 OSSwapInt32
-#define bswap_64 OSSwapInt64
-#include <machine/endian.h>
-#define le16toh(x) OSSwapLittleToHostInt16(x)
-#define le32toh(x) OSSwapLittleToHostInt32(x)
-#define le64toh(x) OSSwapLittleToHostInt64(x)
-#endif
+#include <stdint.h>
+#include <endian.h>
 
-/**
- * struct ieee82011_radiotap_header - base radiotap header
- */
-struct ieee80211_radiotap_header {
-	/**
-	 * @it_version: radiotap version, always 0
-	 */
-	uint8_t it_version;
-
-	/**
-	 * @it_pad: padding (or alignment)
-	 */
-	uint8_t it_pad;
-
-	/**
-	 * @it_len: overall radiotap header length
-	 */
-	uint16_t it_len;
-
-	/**
-	 * @it_present: (first) present word
-	 */
-	uint32_t it_present;
-} __attribute__((__packed__));
-
-/* version is always 0 */
-#define PKTHDR_RADIOTAP_VERSION	0
-
-/* see the radiotap website for the descriptions */
-enum ieee80211_radiotap_presence {
-	IEEE80211_RADIOTAP_TSFT = 0,
-	IEEE80211_RADIOTAP_FLAGS = 1,
-	IEEE80211_RADIOTAP_RATE = 2,
-	IEEE80211_RADIOTAP_CHANNEL = 3,
-	IEEE80211_RADIOTAP_FHSS = 4,
-	IEEE80211_RADIOTAP_DBM_ANTSIGNAL = 5,
-	IEEE80211_RADIOTAP_DBM_ANTNOISE = 6,
-	IEEE80211_RADIOTAP_LOCK_QUALITY = 7,
-	IEEE80211_RADIOTAP_TX_ATTENUATION = 8,
-	IEEE80211_RADIOTAP_DB_TX_ATTENUATION = 9,
-	IEEE80211_RADIOTAP_DBM_TX_POWER = 10,
-	IEEE80211_RADIOTAP_ANTENNA = 11,
-	IEEE80211_RADIOTAP_DB_ANTSIGNAL = 12,
-	IEEE80211_RADIOTAP_DB_ANTNOISE = 13,
-	IEEE80211_RADIOTAP_RX_FLAGS = 14,
-	IEEE80211_RADIOTAP_TX_FLAGS = 15,
-	IEEE80211_RADIOTAP_RTS_RETRIES = 16,
-	IEEE80211_RADIOTAP_DATA_RETRIES = 17,
-	/* 18 is XChannel, but it's not defined yet */
-	IEEE80211_RADIOTAP_MCS = 19,
-	IEEE80211_RADIOTAP_AMPDU_STATUS = 20,
-	IEEE80211_RADIOTAP_VHT = 21,
-	IEEE80211_RADIOTAP_TIMESTAMP = 22,
-
-	/* valid in every it_present bitmap, even vendor namespaces */
-	IEEE80211_RADIOTAP_RADIOTAP_NAMESPACE = 29,
-	IEEE80211_RADIOTAP_VENDOR_NAMESPACE = 30,
-	IEEE80211_RADIOTAP_EXT = 31
+// Массивы каналов и имен с константами
+static const uint32_t ieee80211_radiotap_channels_2GHz[] = {
+    [2412] = 1,
+    [2417] = 2,
+    [2422] = 3,
+    [2427] = 4,
+    [2432] = 5,
+    [2437] = 6,
+    [2442] = 7,
+    [2447] = 8,
+    [2452] = 9,
+    [2457] = 10,
+    [2462] = 11,
+    [2467] = 12,
+    [2472] = 13,
+    [2484] = 14,
 };
 
-/* for IEEE80211_RADIOTAP_FLAGS */
-enum ieee80211_radiotap_flags {
-	IEEE80211_RADIOTAP_F_CFP = 0x01,
-	IEEE80211_RADIOTAP_F_SHORTPRE = 0x02,
-	IEEE80211_RADIOTAP_F_WEP = 0x04,
-	IEEE80211_RADIOTAP_F_FRAG = 0x08,
-	IEEE80211_RADIOTAP_F_FCS = 0x10,
-	IEEE80211_RADIOTAP_F_DATAPAD = 0x20,
-	IEEE80211_RADIOTAP_F_BADFCS = 0x40,
+static const char *ieee80211_radiotap_names[] = {
+    [0] = "TSFT",
+    [1] = "Flags",
+    [2] = "Rate",
+    [3] = "Channel",
+    [4] = "FHSS",
+    [5] = "dbm_Antenna_Signal",
+    [6] = "dbm_Antenna_Noise",
+    [7] = "Lock_Quality",
+    [8] = "TX_Attenuation",
+    [9] = "db_TX_Attenuation",
+    [10] = "dbm_TX_Power",
+    [11] = "Antenna",
+    [12] = "db_Antenna_Signal",
+    [13] = "db_Antenna_Noise",
+    [14] = "RX_Flags",
+    [15] = "TX_Flags",
+    [16] = "RTS_retries",
+    [17] = "Data_retries",
+    [18] = "Channel_plus",
+    [19] = "MCS",
+    [20] = "A_MPDU_Status",
+    [21] = "VHT_Info",
+    [22] = "Frame_timestamp",
+    [23] = "HE_Info",
+    [24] = "HE_MU_Info",
+    [25] = "RESERVED_1",
+    [26] = "Null_Length_PSDU",
+    [27] = "L_SIG",
+    [28] = "TLVs",
+    [29] = "RadioTap_NS_Next",
+    [30] = "Vendor_NS_Next",
+    [31] = "Ext"
 };
 
-/* for IEEE80211_RADIOTAP_CHANNEL */
-enum ieee80211_radiotap_channel_flags {
-	IEEE80211_CHAN_CCK = 0x0020,
-	IEEE80211_CHAN_OFDM = 0x0040,
-	IEEE80211_CHAN_2GHZ = 0x0080,
-	IEEE80211_CHAN_5GHZ = 0x0100,
-	IEEE80211_CHAN_DYN = 0x0400,
-	IEEE80211_CHAN_HALF = 0x4000,
-	IEEE80211_CHAN_QUARTER = 0x8000,
+static const char *ieee80211_radiotap_channel_flags_names[] = {
+    [0] = "700 MHz",
+    [1] = "800 MHz",
+    [2] = "900 MHz",
+    [4] = "Turbo",
+    [5] = "CCK",
+    [6] = "OFDM",
+    [7] = "2 GHz",
+    [8] = "5 GHz",
+    [9] = "Passive",
+    [10] = "Dynamic CCK-OFDM",
+    [11] = "GFSK",
+    [12] = "GSM",
+    [13] = "Static turbo",
+    [14] = "Half-Rate channel 10 MHz",
+    [15] = "Quarter-Rate channel 5 MHz"
 };
 
-/* for IEEE80211_RADIOTAP_RX_FLAGS */
-enum ieee80211_radiotap_rx_flags {
-	IEEE80211_RADIOTAP_F_RX_BADPLCP = 0x0002,
+static const char *ieee80211_radiotap_flags_names[] = {
+    [0] = "CCP",
+    [1] = "Preamble",
+    [2] = "WEP",
+    [3] = "Fragmentation",
+    [4] = "FCS at end",
+    [5] = "Data Pad",
+    [6] = "Bad FCS",
+    [7] = "Short GI"
 };
 
-/* for IEEE80211_RADIOTAP_TX_FLAGS */
-enum ieee80211_radiotap_tx_flags {
-	IEEE80211_RADIOTAP_F_TX_FAIL = 0x0001,
-	IEEE80211_RADIOTAP_F_TX_CTS = 0x0002,
-	IEEE80211_RADIOTAP_F_TX_RTS = 0x0004,
-	IEEE80211_RADIOTAP_F_TX_NOACK = 0x0008,
+static const char *ieee80211_radiotap_tx_flags_names[] = {
+    [0] = "TX Fail",
+    [1] = "TX CTS",
+    [2] = "TX RTS",
+    [3] = "No NACK",
+    [4] = "TX no sequence control"
 };
 
-/* for IEEE80211_RADIOTAP_MCS "have" flags */
-enum ieee80211_radiotap_mcs_have {
-	IEEE80211_RADIOTAP_MCS_HAVE_BW = 0x01,
-	IEEE80211_RADIOTAP_MCS_HAVE_MCS = 0x02,
-	IEEE80211_RADIOTAP_MCS_HAVE_GI = 0x04,
-	IEEE80211_RADIOTAP_MCS_HAVE_FMT = 0x08,
-	IEEE80211_RADIOTAP_MCS_HAVE_FEC = 0x10,
-	IEEE80211_RADIOTAP_MCS_HAVE_STBC = 0x20,
+static const char *ieee80211_radiotap_rx_flags_names[] = {
+    [0] = "RX Bad FCS",
+    [1] = "RX PLCP CRC Fail"
 };
 
-enum ieee80211_radiotap_mcs_flags {
-	IEEE80211_RADIOTAP_MCS_BW_MASK = 0x03,
-	IEEE80211_RADIOTAP_MCS_BW_20 = 0,
-	IEEE80211_RADIOTAP_MCS_BW_40 = 1,
-	IEEE80211_RADIOTAP_MCS_BW_20L = 2,
-	IEEE80211_RADIOTAP_MCS_BW_20U = 3,
+// Структуры данных
+typedef struct {
+    uint8_t it_version;
+    uint8_t it_pad;
+    uint16_t it_len;
+    uint32_t it_present;
+} __attribute__((packed)) ieee80211_radiotap_header;
 
-	IEEE80211_RADIOTAP_MCS_SGI = 0x04,
-	IEEE80211_RADIOTAP_MCS_FMT_GF = 0x08,
-	IEEE80211_RADIOTAP_MCS_FEC_LDPC = 0x10,
-	IEEE80211_RADIOTAP_MCS_STBC_MASK = 0x60,
-	IEEE80211_RADIOTAP_MCS_STBC_1 = 1,
-	IEEE80211_RADIOTAP_MCS_STBC_2 = 2,
-	IEEE80211_RADIOTAP_MCS_STBC_3 = 3,
-	IEEE80211_RADIOTAP_MCS_STBC_SHIFT = 5,
+typedef enum {
+    IEEE80211_RADIOTAP_TSFT                 = 0,
+    IEEE80211_RADIOTAP_Flags                = 1,
+    IEEE80211_RADIOTAP_Rate                 = 2,
+    IEEE80211_RADIOTAP_Channel              = 3,
+    IEEE80211_RADIOTAP_FHSS                 = 4,
+    IEEE80211_RADIOTAP_dbm_Antenna_Signal   = 5,
+    IEEE80211_RADIOTAP_dbm_Antenna_Noise    = 6,
+    IEEE80211_RADIOTAP_Lock_Quality         = 7,
+    IEEE80211_RADIOTAP_TX_Attenuation       = 8,
+    IEEE80211_RADIOTAP_db_TX_Attenuation    = 9,
+    IEEE80211_RADIOTAP_dbm_TX_Power         = 10,
+    IEEE80211_RADIOTAP_Antenna              = 11,
+    IEEE80211_RADIOTAP_db_Antenna_Signal    = 12,
+    IEEE80211_RADIOTAP_db_Antenna_Noise     = 13,
+    IEEE80211_RADIOTAP_RX_Flags             = 14,
+    IEEE80211_RADIOTAP_TX_Flags             = 15,
+    IEEE80211_RADIOTAP_RTS_retries          = 16,
+    IEEE80211_RADIOTAP_Data_retries         = 17,
+    IEEE80211_RADIOTAP_Channel_plus         = 18,
+    IEEE80211_RADIOTAP_MCS                  = 19,
+    IEEE80211_RADIOTAP_A_MPDU_Status        = 20,
+    IEEE80211_RADIOTAP_VHT_Info             = 21,
+    IEEE80211_RADIOTAP_Frame_timestamp      = 22,
+    IEEE80211_RADIOTAP_HE_Info              = 23,
+    IEEE80211_RADIOTAP_HE_MU_Info           = 24,
+    IEEE80211_RADIOTAP_RESERVED_1           = 25,
+    IEEE80211_RADIOTAP_NUL_Length_PSDU      = 26,
+    IEEE80211_RADIOTAP_L_SIG                = 27,
+    IEEE80211_RADIOTAP_TLVs                 = 28,
+    IEEE80211_RADIOTAP_RadioTap_NS_Next     = 29,
+    IEEE80211_RADIOTAP_Vendor_NS_Next       = 30,
+    IEEE80211_RADIOTAP_Ext                  = 31
+} ieee80211_radiotap_present_flags_t;
+
+static const unsigned char ieee80211_radiotap_presents_size[] = {
+    [IEEE80211_RADIOTAP_TSFT]               = 8,
+    [IEEE80211_RADIOTAP_Flags]              = 1,
+    [IEEE80211_RADIOTAP_Rate]               = 1,
+    [IEEE80211_RADIOTAP_Channel]            = 2 * 2,
+    [IEEE80211_RADIOTAP_FHSS]               = 2,
+    [IEEE80211_RADIOTAP_dbm_Antenna_Signal] = 1,
+    [IEEE80211_RADIOTAP_dbm_Antenna_Noise]  = 1,
+    [IEEE80211_RADIOTAP_Lock_Quality]       = 2,
+    [IEEE80211_RADIOTAP_TX_Attenuation]     = 2,
+    [IEEE80211_RADIOTAP_db_TX_Attenuation]  = 2,
+    [IEEE80211_RADIOTAP_dbm_TX_Power]       = 1,
+    [IEEE80211_RADIOTAP_Antenna]            = 1,
+    [IEEE80211_RADIOTAP_db_Antenna_Signal]  = 1,
+    [IEEE80211_RADIOTAP_db_Antenna_Noise]   = 1,
+    [IEEE80211_RADIOTAP_RX_Flags]           = 2,
+    [IEEE80211_RADIOTAP_TX_Flags]           = 2,
+    [IEEE80211_RADIOTAP_RTS_retries]        = 1,
+    [IEEE80211_RADIOTAP_Data_retries]       = 1,
+    [IEEE80211_RADIOTAP_MCS]                = 1 + 1 + 1,
+    [IEEE80211_RADIOTAP_A_MPDU_Status]      = 4 + 2 + 1 + 1,
+    [IEEE80211_RADIOTAP_VHT_Info]           = 12,
+    [IEEE80211_RADIOTAP_Frame_timestamp]    = 12
 };
 
-/* for IEEE80211_RADIOTAP_AMPDU_STATUS */
-enum ieee80211_radiotap_ampdu_flags {
-	IEEE80211_RADIOTAP_AMPDU_REPORT_ZEROLEN = 0x0001,
-	IEEE80211_RADIOTAP_AMPDU_IS_ZEROLEN = 0x0002,
-	IEEE80211_RADIOTAP_AMPDU_LAST_KNOWN = 0x0004,
-	IEEE80211_RADIOTAP_AMPDU_IS_LAST = 0x0008,
-	IEEE80211_RADIOTAP_AMPDU_DELIM_CRC_ERR = 0x0010,
-	IEEE80211_RADIOTAP_AMPDU_DELIM_CRC_KNOWN = 0x0020,
+static const unsigned char ieee80211_radiotap_presents_align[] = {
+    [IEEE80211_RADIOTAP_TSFT]               = 8,
+    [IEEE80211_RADIOTAP_Flags]              = 1,
+    [IEEE80211_RADIOTAP_Rate]               = 1,
+    [IEEE80211_RADIOTAP_Channel]            = 2,
+    [IEEE80211_RADIOTAP_FHSS]               = 2,
+    [IEEE80211_RADIOTAP_dbm_Antenna_Signal] = 1,
+    [IEEE80211_RADIOTAP_dbm_Antenna_Noise]  = 1,
+    [IEEE80211_RADIOTAP_Lock_Quality]       = 2,
+    [IEEE80211_RADIOTAP_TX_Attenuation]     = 2,
+    [IEEE80211_RADIOTAP_db_TX_Attenuation]  = 2,
+    [IEEE80211_RADIOTAP_dbm_TX_Power]       = 1,
+    [IEEE80211_RADIOTAP_Antenna]            = 1,
+    [IEEE80211_RADIOTAP_db_Antenna_Signal]  = 1,
+    [IEEE80211_RADIOTAP_db_Antenna_Noise]   = 1,
+    [IEEE80211_RADIOTAP_RX_Flags]           = 2,
+    [IEEE80211_RADIOTAP_TX_Flags]           = 2,
+    [IEEE80211_RADIOTAP_RTS_retries]        = 1,
+    [IEEE80211_RADIOTAP_Data_retries]       = 1,
+    [IEEE80211_RADIOTAP_MCS]                = 1,
+    [IEEE80211_RADIOTAP_A_MPDU_Status]      = 4,
+    [IEEE80211_RADIOTAP_VHT_Info]           = 2,
+    [IEEE80211_RADIOTAP_Frame_timestamp]    = 8
 };
 
-/* for IEEE80211_RADIOTAP_VHT */
-enum ieee80211_radiotap_vht_known {
-	IEEE80211_RADIOTAP_VHT_KNOWN_STBC = 0x0001,
-	IEEE80211_RADIOTAP_VHT_KNOWN_TXOP_PS_NA = 0x0002,
-	IEEE80211_RADIOTAP_VHT_KNOWN_GI = 0x0004,
-	IEEE80211_RADIOTAP_VHT_KNOWN_SGI_NSYM_DIS = 0x0008,
-	IEEE80211_RADIOTAP_VHT_KNOWN_LDPC_EXTRA_OFDM_SYM = 0x0010,
-	IEEE80211_RADIOTAP_VHT_KNOWN_BEAMFORMED = 0x0020,
-	IEEE80211_RADIOTAP_VHT_KNOWN_BANDWIDTH = 0x0040,
-	IEEE80211_RADIOTAP_VHT_KNOWN_GROUP_ID = 0x0080,
-	IEEE80211_RADIOTAP_VHT_KNOWN_PARTIAL_AID = 0x0100,
-};
+typedef union {
+	struct {
+		uint8_t CFP:1;
+		uint8_t Preamble:1;
+		uint8_t WEP:1;
+		uint8_t Fragmentation:1;
+		uint8_t FCS_at_END:1;
+		uint8_t Data_Pad:1;
+		uint8_t Bad_FCS:1;
+		uint8_t Short_GI:1;
+	};
+	uint8_t value;
+} ieee80211_radiotap_flags_t;
 
-enum ieee80211_radiotap_vht_flags {
-	IEEE80211_RADIOTAP_VHT_FLAG_STBC = 0x01,
-	IEEE80211_RADIOTAP_VHT_FLAG_TXOP_PS_NA = 0x02,
-	IEEE80211_RADIOTAP_VHT_FLAG_SGI = 0x04,
-	IEEE80211_RADIOTAP_VHT_FLAG_SGI_NSYM_M10_9 = 0x08,
-	IEEE80211_RADIOTAP_VHT_FLAG_LDPC_EXTRA_OFDM_SYM = 0x10,
-	IEEE80211_RADIOTAP_VHT_FLAG_BEAMFORMED = 0x20,
-};
+typedef union {
+    struct {
+        uint16_t MHz_700:1;  // 1 бит
+        uint16_t MHz_800:1;  // 1 бит
+        uint16_t MHz_900:1;  // 1 бит
+        uint16_t Turbo:1;    // 1 бит
+        uint16_t CCK:1;      // 1 бит
+        uint16_t OFDM:1;     // 1 бит
+        uint16_t GHz_2:1;    // 1 бит
+        uint16_t GHz_5:1;    // 1 бит
+        uint16_t Passive:1;  // 1 бит
+        uint16_t Dynamic_CCK_OFDM:1; // 1 бит
+        uint16_t GFSK:1;     // 1 бит
+        uint16_t GSM:1;      // 1 бит
+        uint16_t Static_turbo:1; // 1 бит
+        uint16_t Half_rate_channel_10Mhz:1; // 1 бит
+        uint16_t Quarter_rate_channel_5Mhz:1; // 1 бит
+        uint16_t reserved:1; // 1 бит
+    };
+    uint16_t value; // Общий доступ ко всем битам
+} ieee80211_radiotap_channel_flags_t;
 
-enum ieee80211_radiotap_vht_coding {
-	IEEE80211_RADIOTAP_CODING_LDPC_USER0 = 0x01,
-	IEEE80211_RADIOTAP_CODING_LDPC_USER1 = 0x02,
-	IEEE80211_RADIOTAP_CODING_LDPC_USER2 = 0x04,
-	IEEE80211_RADIOTAP_CODING_LDPC_USER3 = 0x08,
-};
 
-/* for IEEE80211_RADIOTAP_TIMESTAMP */
-enum ieee80211_radiotap_timestamp_unit_spos {
-	IEEE80211_RADIOTAP_TIMESTAMP_UNIT_MASK = 0x000F,
-	IEEE80211_RADIOTAP_TIMESTAMP_UNIT_MS = 0x0000,
-	IEEE80211_RADIOTAP_TIMESTAMP_UNIT_US = 0x0001,
-	IEEE80211_RADIOTAP_TIMESTAMP_UNIT_NS = 0x0003,
-	IEEE80211_RADIOTAP_TIMESTAMP_SPOS_MASK = 0x00F0,
-	IEEE80211_RADIOTAP_TIMESTAMP_SPOS_BEGIN_MDPU = 0x0000,
-	IEEE80211_RADIOTAP_TIMESTAMP_SPOS_PLCP_SIG_ACQ = 0x0010,
-	IEEE80211_RADIOTAP_TIMESTAMP_SPOS_EO_PPDU = 0x0020,
-	IEEE80211_RADIOTAP_TIMESTAMP_SPOS_EO_MPDU = 0x0030,
-	IEEE80211_RADIOTAP_TIMESTAMP_SPOS_UNKNOWN = 0x00F0,
-};
+typedef union {
+    struct {
+        uint16_t tx_fail:1;
+        uint16_t cts:1;
+        uint16_t rts:1;
+        uint16_t nonack:1;
+        uint16_t noseq:1;
+        uint16_t reserved_1:3; // 3 бита в запасе, чтобы добить до 8
+    };
+    uint8_t value;
+} ieee80211_radiotap_tx_flags_t;
 
-enum ieee80211_radiotap_timestamp_flags {
-	IEEE80211_RADIOTAP_TIMESTAMP_FLAG_64BIT = 0x00,
-	IEEE80211_RADIOTAP_TIMESTAMP_FLAG_32BIT = 0x01,
-	IEEE80211_RADIOTAP_TIMESTAMP_FLAG_ACCURACY = 0x02,
-};
+typedef union {
+    struct {
+        uint16_t bad_fcs:1;
+        uint16_t plcp_crc:1;
+        uint16_t reserved_1:6; // Оставляем 6 бит запаса до 8
+    };
+    uint8_t value;
+} ieee80211_radiotap_rx_flags_t;
 
-#endif /* __RADIOTAP_H */
+typedef union {
+    struct {
+        uint8_t have_bw:1;
+        uint8_t have_mcs:1;
+        uint8_t have_gi:1;
+        uint8_t have_fmt:1;
+        uint8_t have_fec:1;
+        uint8_t reserved_1:3; // 3 бита запас
+    };
+    uint8_t value;
+} ieee80211_radiotap_mcs_known_t;
+
+typedef union {
+    struct {
+        uint8_t bw_40:1;
+        uint8_t bw20l:1;
+        uint8_t bw20u:1;
+        uint8_t short_gi:1;
+        uint8_t fmt_gf:1;
+        uint8_t fec_ldpc:1;
+        uint8_t reserved_1:2; // 2 бита запас
+    };
+    uint8_t value;
+} ieee80211_radiotap_mcs_flags_t;
+
+typedef struct {
+    ieee80211_radiotap_mcs_known_t known;
+    ieee80211_radiotap_mcs_flags_t flags;
+    uint8_t mcs_index;
+} ieee80211_radiotap_mcs_t;
+
+typedef struct {
+	ieee80211_radiotap_header *rt_header;
+    uint64_t tsft;
+    ieee80211_radiotap_flags_t flags;
+    double rate;
+    uint16_t channel_frequency;
+    uint16_t channel;
+    ieee80211_radiotap_channel_flags_t channel_flags;
+    int8_t dbm_Antenna_Signal;
+    uint8_t antenna;
+    ieee80211_radiotap_tx_flags_t tx_flags;
+    ieee80211_radiotap_rx_flags_t rx_flags;
+    ieee80211_radiotap_mcs_t mcs;
+} ieee80211_radiotap_data_t;
+
+extern int radiotap_parse(const uint8_t *packet, ieee80211_radiotap_data_t *data);
+
+#endif // IEEE80211_RADIOTAP_H
